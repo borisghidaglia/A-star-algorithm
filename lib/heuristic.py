86cf82efbab60f7043ace35ff9751aa62c3c5bf1
Augustin <augustin.pro@gmail.com>
2018-04-15 19:51:20 +0200

    Created new heuristics, started working on test matrices

--- lib/heuristic.py
+++ lib/heuristic.py
@@ -1,13 +1,49 @@
 def null_heuristic(a_star, node):
     return 0
 
+
+def weighted_min(a_star, node):
+    available_agents = a_star.agents - node.attributed_agents
+    if not available_agents:
+        return 0
+    node_job = node.job
+    val_min = float('inf')
+    for job in range(node_job+1, a_star.cost_matrix.shape[0]):
+        for agent in available_agents:
+            val_min = min(val_min, a_star.cost_matrix[job][agent])
+    return val_min*len(available_agents)
+
+
 def sum_min_by_rows(a_star, node):
     available_agents = a_star.agents - node.attributed_agents
-    job = node.job
+    if not available_agents:
+        return 0
+    node_job = node.job
     val_heuristic = 0
-    for job in range(job+1, a_star.cost_matrix.shape[0]):
+    for job in range(node_job+1, a_star.cost_matrix.shape[0]):
         val_min = float('inf')
         for agent in available_agents:
             val_min = min(val_min, a_star.cost_matrix[job][agent])
         val_heuristic += val_min
     return val_heuristic
+
+
+def sum_min_by_cols(a_star, node):
+    available_agents = a_star.agents - node.attributed_agents
+    if not available_agents:
+        return 0
+    node_job = node.job
+    val_heuristic = 0
+    for agent in available_agents:
+        val_min = float('inf')
+        for job in range(node_job+1, a_star.cost_matrix.shape[0]):
+            val_min = min(val_min, a_star.cost_matrix[job][agent])
+        val_heuristic += val_min
+    return val_heuristic
+
+
+def max_sum_min_by_r_and_c(a_star, node):
+    return max(
+        sum_min_by_rows(a_star, node),
+        sum_min_by_cols(a_star, node)
+        )
--- lib/main.py
+++ lib/main.py
@@ -1,5 +1,5 @@
 from A_star_tree import A_star_tree
-from heuristic import null_heuristic, sum_min_in_lines
+from heuristic import null_heuristic, sum_min_by_rows, sum_min_by_cols, max_sum_min_by_r_and_c, weighted_min
 import numpy as np
 import time
 
@@ -9,12 +9,12 @@ def get_random_matrix(dim, min_val=1, max_val=11):
 cost_matrix = get_random_matrix(5)
 print(cost_matrix)
 
-for fun in (null_heuristic, sum_min_in_lines):
+for fun in (null_heuristic, sum_min_by_rows, sum_min_by_cols, max_sum_min_by_r_and_c, weighted_min):
     tree = A_star_tree(cost_matrix, fun)
     start_time = time.time()
     path = tree.get_bests_affectations()
+    end_time = time.time()
     print('\nCost : %s' %path[-1].g)
-    print('H : %s' %path[-1].heuristic)
     print(' --> '.join((str(el) for el in path)))
-    print(time.time() - start_time)
+    print(end_time - start_time)
     print(tree.visited)
